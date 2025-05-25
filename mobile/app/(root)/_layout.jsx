24b7ff54fffd8cb3cc960e12f2ae24dc41691e2f
Greg Delgado <grd888@gmail.com>
2025-05-25 20:27:25 +0800

    layout fixes

--- mobile/app/(root)/_layout.jsx
+++ mobile/app/(root)/_layout.jsx
@@ -1,11 +1,10 @@
 import { useUser } from "@clerk/clerk-expo";
-import { Redirect } from "expo-router";
-import { Stack } from "expo-router/stack";
+import { Redirect, Stack } from "expo-router";
 
 export default function Layout() {
-  const { isSignedIn } = useUser()
+  const { isSignedIn } = useUser();
 
-  if (!isSignedIn) return <Redirect href={'/sign-in'} />
+  if (!isSignedIn) return <Redirect href={"/sign-in"} />;
 
-  return <Stack screenOptions={{headerShown: false}}/>
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
