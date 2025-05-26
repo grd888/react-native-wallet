3ba5e87f585b694fa7d6492be8b43503b1346797
Greg Delgado <grd888@gmail.com>
2025-05-26 23:15:36 +0800

    create useTransactions hook

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -1,11 +1,21 @@
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
-import { SignOutButton } from '@/components/SignOutButton'
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import { SignOutButton } from "@/components/SignOutButton";
+import { useTransactions } from "../hooks/useTransactions";
+import { useEffect } from "react";
 
 export default function Page() {
-  const { user } = useUser()
+  const { user } = useUser();
+  const [transactions, summary, isLoading, loadData, deleteTransaction] =
+    useTransactions(user.id);
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
 
+  console.log("transactions", transactions);
+  console.log("Summary", summary);
+  
   return (
     <View>
       <SignedIn>
@@ -21,5 +31,5 @@ export default function Page() {
         </Link>
       </SignedOut>
     </View>
-  )
+  );
 }
